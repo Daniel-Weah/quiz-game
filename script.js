740e96cd6761d5ef08cf8a59dad99e44ceacd600
Daniel K. Weah, Jr <weahjrdanielk@gmail.com>
2024-07-02 07:55:42 +0000

    commited

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('btn_1').addEventListener('click', (e) => {
-    window.location.href='/HTML/HTML_Quiz.html';
+    window.location.href='HTML_Quiz.html';
 });
 document.getElementById('btn_2').addEventListener('click', (e) => {
-    window.location.href='/HTML/CSS_Quiz.html';
+    window.location.href='CSS_Quiz.html';
 });
 document.getElementById('btn_3').addEventListener('click', (e) => {
-    window.location.href='/HTML/JS_Quiz.html';
+    window.location.href='JS_Quiz.html';
 });
 document.getElementById('btn_4').addEventListener('click', (e) => {
-    window.location.href='/HTML/ALL_Quiz.html';
+    window.location.href='ALL_Quiz.html';
 });
