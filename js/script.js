7050603758c0542d42f892163347f311523de66b
Daniel K. Weah, Jr <weahjrdanielk@gmail.com>
2024-07-02 07:48:16 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('btn_1').addEventListener('click', (e) => {
+    window.location.href='/HTML/HTML_Quiz.html';
+});
+document.getElementById('btn_2').addEventListener('click', (e) => {
+    window.location.href='/HTML/CSS_Quiz.html';
+});
+document.getElementById('btn_3').addEventListener('click', (e) => {
+    window.location.href='/HTML/JS_Quiz.html';
+});
+document.getElementById('btn_4').addEventListener('click', (e) => {
+    window.location.href='/HTML/ALL_Quiz.html';
+});
